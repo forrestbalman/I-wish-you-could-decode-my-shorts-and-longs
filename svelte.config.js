cb4ec6e4c2cfe27d29e10a53a54df16a4e679618
forrestbalman <ifork1@gmail.com>
2024-03-14 19:51:08 -0700

    vite test

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+    preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter({
-            fallback: "404.html",
-        }),
+        adapter: adapter(),
         paths: {
-            base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+            base: dev ? "" : process.env.BASE_PATH,
         },
     },
 };
