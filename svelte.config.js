97d7661938b22b535248403c2d77b489450f080c
forrestbalman <ifork1@gmail.com>
2024-03-14 19:09:40 -0700

    file changes 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: dev ? "" : process.env.BASE_PATH,
+        },
+    },
 };
 
 export default config;
