757c7c597ed93f74d259386e01a335b5183c58c2
forrestbalman <ifork1@gmail.com>
2024-03-14 19:17:25 -0700

    changed commit type

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
-
-const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            fallback: "404.html",
+        }),
         paths: {
-            base: dev ? "" : process.env.BASE_PATH,
+            base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
         },
     },
 };
